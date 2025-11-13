a3e0f08f0a238abc43ed7d9e45d81f751a2c4f4a
Jonathan Ndambuki <joendambuki16@gmail.com>
2025-11-13 16:12:39 +0300

    Added Errors

--- Routes/index.js
+++ Routes/index.js
@@ -5,8 +5,8 @@ const {  getScrapped, getDetailedScrapped } = require("../controller")
 const puppetRouter = Router()
 
 
-puppetRouter.post("", getScrapped)
-puppetRouter.post("/detailed", getDetailedScrapped)
+puppetRouter.get("", getScrapped)
+puppetRouter.get("/detailed", getDetailedScrapped)
 
 
 module.exports={puppetRouter}
