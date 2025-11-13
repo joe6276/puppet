f2f0f969f080bc8565b586bee47d649d38f74778
jonathan Ndambuki <joendambuki16@gmail.com>
2025-11-13 21:43:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.use(json())
 app.use('/scrap', puppetRouter)
 
 
+app.get("/test", (req,res)=>{
+    res.status(200).send("<h1> Hello Joe !!!! </h1>")
+})
+
+
  app.listen(80, ()=>{
     console.log("Server is running on port 80")
  })
